8579c85cc08e49c315b17212e511b0d0961b66a1
maurociancio <maurociancio@33273102-b53b-426a-a223-73ecc30432d0>
2010-02-18 01:40:37 +0000

    add wicket utils project
    git-svn-id: https://repo.noxit.com.ar/svn/mavenrepo/trunk@15 33273102-b53b-426a-a223-73ecc30432d0

---
+++ wicket-utils/src/main/java/ar/noxit/web/wicket/StaticImage.java
@@ -0,0 +1,54 @@
+package ar.noxit.web.wicket;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebComponent;
+import org.apache.wicket.model.IModel;
+
+/**
+ * A static image that renders its src tag from a model
+ * 
+ * @author Mauro Ciancio
+ * 
+ */
+public class StaticImage extends WebComponent {
+
+    /**
+     * src's model
+     */
+    private IModel<String> model = null;
+
+    /**
+     * 
+     * @param id
+     *            component id
+     * @param model
+     *            to get the src's url from
+     */
+    public StaticImage(String id, IModel<String> model) {
+        super(id);
+        if (model == null) {
+            throw new IllegalArgumentException("model cannot be null");
+        }
+
+        this.model = model;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onComponentTag(ComponentTag tag) {
+        super.onComponentTag(tag);
+        checkComponentTag(tag, "img");
+        tag.put("src", model.toString());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onDetach() {
+        super.onDetach();
+        model.detach();
+    }
+}
