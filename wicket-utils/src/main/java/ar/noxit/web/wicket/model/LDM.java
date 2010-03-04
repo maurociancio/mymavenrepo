cde35dba5fdeb04d07ffa96f35b16cd4d42c9205
maurociancio <maurociancio@33273102-b53b-426a-a223-73ecc30432d0>
2010-03-04 04:28:40 +0000

    agregar constructor a ldm
    git-svn-id: https://repo.noxit.com.ar/svn/mavenrepo/trunk@51 33273102-b53b-426a-a223-73ecc30432d0

--- wicket-utils/src/main/java/ar/noxit/web/wicket/model/LDM.java
+++ wicket-utils/src/main/java/ar/noxit/web/wicket/model/LDM.java
@@ -1,12 +1,17 @@
 package ar.noxit.web.wicket.model;
 
 import ar.noxit.exceptions.NoxitRuntimeException;
-
 import ar.noxit.exceptions.NoxitException;
-
 import org.apache.wicket.model.LoadableDetachableModel;
 
 public abstract class LDM<T> extends LoadableDetachableModel<T> {
+
+    public LDM() {
+    }
+
+    public LDM(T object) {
+        super(object);
+    }
 
     @Override
     protected T load() {
