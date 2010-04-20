66efbe78fc53564c385d1006b05d59de10b16587
maurociancio <maurociancio@33273102-b53b-426a-a223-73ecc30432d0>
2010-04-20 15:12:47 +0000

    date 2 localdatetime adapter model
    git-svn-id: https://repo.noxit.com.ar/svn/mavenrepo/trunk@86 33273102-b53b-426a-a223-73ecc30432d0

---
+++ src/wicket-utils/src/main/java/ar/noxit/web/wicket/model/Date2LocalDateTimeAdapterModel.java
@@ -0,0 +1,46 @@
+package ar.noxit.web.wicket.model;
+
+import java.util.Calendar;
+import java.util.Date;
+import org.apache.wicket.model.IModel;
+import org.joda.time.LocalDateTime;
+
+/**
+ * Adapter Model that converts from Date to LocalDateTime
+ * 
+ * @author Mauro Ciancio
+ * 
+ */
+public class Date2LocalDateTimeAdapterModel extends AdapterModel<Date, LocalDateTime> {
+
+    public Date2LocalDateTimeAdapterModel(IModel<LocalDateTime> delegate) {
+        super(delegate);
+    }
+
+    @Override
+    protected Date getObject(IModel<LocalDateTime> delegate) {
+        LocalDateTime ldt = delegate.getObject();
+        if (ldt == null)
+            return null;
+
+        int year = ldt.getYear();
+        int monthOfYear = ldt.getMonthOfYear();
+        int dayOfMonth = ldt.getDayOfMonth();
+        int hourOfDay = ldt.getHourOfDay();
+        int minuteOfHour = ldt.getMinuteOfHour();
+        int secondOfMinute = ldt.getSecondOfMinute();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, monthOfYear, dayOfMonth, hourOfDay, minuteOfHour, secondOfMinute);
+        return calendar.getTime();
+    }
+
+    @Override
+    protected void setObject(Date object, IModel<LocalDateTime> delegate) {
+        if (object == null) {
+            delegate.setObject(null);
+        } else {
+            delegate.setObject(new LocalDateTime(object));
+        }
+    }
+}
